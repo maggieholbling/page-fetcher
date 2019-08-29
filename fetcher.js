e2cdd76017263e8d8da75460ad147bf604dee7f1
Maggie <maggieholbl@gmail.com>
2019-08-29 04:05:12 +0000

    added error messages

--- fetcher.js
+++ fetcher.js
@@ -5,18 +5,19 @@ const fetcher = (url, localPath) => {
   request(url, (error, response, body) => {
     const text = body;
     if (error) throw error;
-    if (response.statusCode !== 200) return console.log(`Error: ${response && response.statusCode}`);
+    if (response.statusCode !== 200) return console.log(`Error: ${response.statusCode}`);
     
     write(localPath, text);
   });
 };
 
 const write = (localFilePath, content) => {
-  const stats = fs.statSync(localFilePath);
-  const fileSizeInBytes = stats.size;
+  
 
   fs.writeFile(localFilePath, content, function (err) {
-    if (err) throw err;
+    if (err) return console.log(`Cannot write to file ${localFilePath}`);
+    const stats = fs.statSync(localFilePath);
+    const fileSizeInBytes = stats.size;
     console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${localFilePath}`);
   });
 }
